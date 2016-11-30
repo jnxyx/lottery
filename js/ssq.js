22bcde33e0cf5da20f6fc2dc200cf67eb8c128ae
jnxyx <821969066@qq.com>
2016-11-30 16:52:17 +0800

    update

--- js/ssq.js
+++ js/ssq.js
@@ -1,8 +1,8 @@
 var ssq = {
 
     init: function() {
-    	ssq.initChose();
-    },	
+        ssq.initChose();
+    },
 
     // 初始化选球区
     initChose: function() {
@@ -11,24 +11,44 @@ var ssq = {
         $('#chose').append('<div id="redContainer"></div><div id="blueContainer"></div>')
 
         // 初始化红球选区
-        var redElement, redElementHtml = '<span></span>';
+        var redElement, redElementHtml = '<span class="redSpan"></span>';
 
         for (var red = 1; red < 34; red++) {
             redElement = $(redElementHtml);
             redElement.attr('code', red);
             redElement.text('' + red);
+            redElement.click(function() {
+                if ($(this).hasClass('active')) {
+                    $(this).removeClass('active');
+                } else {
+                    $(this).addClass('active');
+                }
+            });
             $('#redContainer').append(redElement);
         }
 
         // 初始化蓝球选区
-        var blueElement, blueElementHtml = '<span></span>';
+        var blueElement, blueElementHtml = '<span class="blueSpan"></span>';
 
         for (var blue = 1; blue < 17; blue++) {
             blueElement = $(blueElementHtml);
             blueElement.attr('code', blue);
             blueElement.text('' + blue);
+            blueElement.click(function() {
+                if ($(this).hasClass('active')) {
+                    $(this).removeClass('active');
+                } else {
+                    $(this).addClass('active');
+                }
+            });
             $('#blueContainer').append(blueElement);
         }
+    },
+
+    getChose: function() {
+        var chose = $('.active').clone();
+
+        $('.chosed').append(chose);
     }
 };
 
