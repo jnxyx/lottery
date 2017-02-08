58e026cdbfa375e2e4bfc7184900ed2ef2f4e8c1
jnxyx <821969066@qq.com>
2017-02-08 17:41:02 +0800

    test

--- js/data.js
+++ js/data.js
@@ -6,3 +6,20 @@ var realData = [
     [],
     [],
 ];
+
+var areaData = {
+    add: [{
+        areas_first: 52
+    }, {
+        areas_first: 52,
+        areas_second: 567,
+        areas_third: 3557
+    }],
+    del: [{
+        areas_first: 52
+    }, {
+        areas_first: 52,
+        areas_second: 567,
+        areas_third: 3557
+    }]
+}
